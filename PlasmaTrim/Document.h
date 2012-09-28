64d1e5227c495cdcc70f70e1facb2f3d880a0db5
Kent Sutherland <git@ksuther.com>
2012-09-28 11:13:46 -0400

    Initial Commit

---
+++ PlasmaTrim/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  PlasmaTrim
+//
+//  Created by Kent Sutherland on 9/28/12.
+//  Copyright (c) 2012 Kent Sutherland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ PlasmaTrim/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PlasmaTrim
+//
+//  Created by Kent Sutherland on 9/28/12.
+//  Copyright (c) 2012 Kent Sutherland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
